c28d394afe7a040a0ae44f632cd0df18763a8a0d
(っ◔◡◔)っ ♥ bhanu ♥ <83598884+bhanuprasadsamal@users.noreply.github.com>
2023-11-30 09:59:02 +0530

    My first django project

---
+++ blog/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    # path('',views.test),
+    # path('loginn/',views.loginn),
+    # path('',views.signup),
+    # path('home/', views.home)
+    path('', views.signup, name='signup-page'),
+    path('loginn/', views.loginn, name='login-page'),
+    path('home/', views.home, name='home-page'),
+    path('newpost/', views.newPost, name='new-post'),
+    path('mypost/', views.myPost, name='my-post'),
+    path('signout/', views.signout, name='signout-btn'),
+]
